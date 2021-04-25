175fa7d46ce8dedf07e8de311a0c9e37e90bf7b8
Xinran Gao <sherrygao821@hotmail.com>
2021-04-25 23:11:59 +0800

    Update PostFeed.js

--- src/components/PostFeed.js
+++ src/components/PostFeed.js
@@ -1,55 +1,18 @@
-import axios from 'axios';
+import Axios from 'axios';
 import React, { Component } from 'react';
 import Post from '../components/Post';
 
 class PostFeed extends Component{
     state = {
-        posts : [
-            {
-                postID:"21961bfa-8973-4a70-93cf-21c1aa7ee632", 
-                tags:['#mentalHealth', '#help'], 
-                content:"Just had an argument with my parents...blah", 
-                likedCount:0,
-                comments:[
-                    {
-                        anonymousPosterName: "baby pig",
-                        content: "hi i am baby pig"
-                    },
-                    {
-                        anonymousPosterName: "baby cow",
-                        content: "hi i am baby cow"
-                    },
-                ],
-                anonymousPosterName:"zebra",
-                userUuid: 1,
-             },
-             {
-                postID:"af59878c-9870-4bf5-a553-c7b2320bde2b", 
-                tags:['#mentalhealth'], 
-                content:"More...blah", 
-                likedCount:0,
-                comments:[
-                    {
-                        anonymousPosterName: "baby pig",
-                        content: "what the hell"
-                    },
-                    {
-                        anonymousPosterName: "baby cow",
-                        content: "damn u react!!!"
-                    },
-                ],
-                anonymousPosterName:"some animal",
-                userUuid: 2,
-             }
-        ]
+        posts : {status: false}
     }
 
-    /*
+    
     async componentDidMount() {
         // pending > resolved (success) OR rejected (failure)
         const { data: posts } = await Axios.get("http://35.236.53.120:3000/api/post/get");
         this.setState({ posts });
-      }
+    }
 
     
     handleAdd = async () => {
@@ -64,7 +27,7 @@ class PostFeed extends Component{
         const posts = [post, ...this.state.posts];
         this.setState({ posts });
     };
-    */
+    
     
     
 
@@ -73,11 +36,14 @@ class PostFeed extends Component{
         // TODO: receive json data of the feed from backend
         
         let items = [];
-        console.log(this.state.posts.data);
+        console.log(this.state.posts);
+        // console.log(this.state.posts.data);
         
-        
-        for(var i = 0; i < this.state.posts.length; i++){
-            items.push(<Post post = {this.state.posts[i]}/>) 
+        if(this.state.posts.status == true){
+            console.log("length:" + this.state.posts.data.length);
+            for(var i = 0; i < this.state.posts.data.length; i++){
+                items.push(<Post post = {this.state.posts.data[i]}/>) 
+            }
         }
         
         
