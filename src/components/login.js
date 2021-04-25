3483ace01b57c8df585f90e465a9ac36d14bd751
Kevin Ma <kyma@usc.edu>
2021-04-25 22:00:34 +0800

    login

--- src/components/login.js
+++ src/components/login.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+
 
 class login extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-            data:{
-                email: '',
-                psw: ''
-            }
+            email: '',
+            psw: '',
+            uuid: '',
+            usename: '',
+            postIDs: [],
+            likedPostIDs: []
+
         }
+        // this.changeHandler = this.changeHandler.bind(this);
     }
 
 
@@ -20,14 +24,39 @@ class login extends Component {
 
     submitHandler = (e) =>{
         e.preventDefault()
-        console.log(this.state.data)
-        axios.get('http://35.236.53.120:3000/api/auth/login', this.state.data)
-        .then(response=>{
-            console.log(response)
+        // console.log(this.state.data)
+        const axios = require('axios');
+        var self = this;
+        axios.get('http://35.236.53.120:3000/api/auth/login?email='
+        + this.state.email + '&psw=' + this.state.psw, {
+            headers:{
+                'Content-Type': 'application/json'
+            }
         })
-        .catch(error=>{
-            console.log(error)
+        .then(function (response) {
+            console.log('hi')
+            
+            console.log(response.data.data.uuid)
+            self.setState({
+                uuid: response.data.data.uuid,
+                postIDs: response.data.data.postIDs,
+                likedPostIDs: response.data.data.likedPostIDs
+            })
+            
         })
+        .catch(function (error) {
+            // console.log(error)
+            // console.log(error.request)
+            // console.log(error.response)
+            // const obj = JSON.parse(error.response)
+        })
+        .then(function (){
+            console.log(self.state)
+            sessionStorage.setItem('uuid', self.state.uuid)
+            sessionStorage.setItem('postIDs', self.state.postIDs)
+            sessionStorage.setItem('likedPostIDs', self.state.likedPostIDs)
+            console.log(sessionStorage.getItem('uuid'))
+        });
     }
     
     render() {
@@ -39,7 +68,7 @@ class login extends Component {
                         <input type="text" name="email" value={email} onChange={this.changeHandler}/>
                     </div>
                     <div>
-                        <input type="text" name="psw" value={psw} onChange={this.changeHandler}/>
+                        <input type="password" name="psw" value={psw} onChange={this.changeHandler}/>
                     </div>
                     <div>
                         <button type="submit">Submit</button>
